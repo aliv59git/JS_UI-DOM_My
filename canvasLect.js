836dabed7f017764f3dd059902a10437747e5ba2
aliv59git <vilavaln@gmail.com>
2015-07-20 16:39:39 +0300

    new course

---
+++ canvasLect.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+	var theCanvas = document.getElementById("the-canvas");
+	var canvasCtx = theCanvas.getContext("2d");
+
+	canvasCtx.lineWidth = "5";
+	canvasCtx.fillStyle = "#ccc";
+	canvas.Ctx.strokeStyle = "#777";
+
+
+
+
+
+
+}
