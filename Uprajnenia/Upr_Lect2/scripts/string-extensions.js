c2b702b59293f2f15d48cd403e0f7da0981f3eaf
aliv59git <vilavaln@gmail.com>
2015-07-21 16:41:29 +0300

    nreasdfa

---
+++ Uprajnenia/Upr_Lect2/scripts/string-extensions.js
@@ -0,0 +1,18 @@
+(function (){
+  if (!String.prototype.repeat) {
+    String.prototype.repeat = function (times){
+      var repeatedString;
+      if(!times){
+        times = 1;  
+      }
+      //times = times || 1;
+      repeatedString = "";
+
+      for (var i = 0; i < times; i+=1) {
+        repeatedString += String(this);
+      }
+      return repeatedString;
+    };
+  }
+
+}());
