e734f2413eeae9ce34a4185fb520c27d403d97b0
aliv59git <vilavaln@gmail.com>
2015-07-22 16:31:48 +0300

    new vers

---
+++ Uprajnenia/Upr_Lect3/scripts/drawing-text.js
@@ -0,0 +1,19 @@
+(function (){
+  var canvas = document.getElementById('the-canvas'),
+    ctx = canvas.getContext('2d'),
+    text,
+    x = 50,
+    y = 50,
+    minFontSize,
+    fontFamily,
+    fillColor,
+    strokeColor,
+    currentFontSize,
+    offset;
+
+  offset = 5;
+  minFontSize = '28';
+
+
+
+}());
