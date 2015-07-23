ef497222ac91fb1468e6b362c11b09b91cc42ccc
aliv59git <vilavaln@gmail.com>
2015-07-24 00:23:52 +0300

    new ffff

--- Uprajnenia/Upr_Lect3/scripts/per-pixel-manipulations.js
+++ Uprajnenia/Upr_Lect3/scripts/per-pixel-manipulations.js
@@ -1,12 +1,13 @@
 function invertColorButtonClick(){
-  var ctx = document.getElementsByTagName('canvas')[0].getContext('2d');
-  var imageData = ctx.getImgeData(0, 0, ctx.canvas.width, ctx.canvas.height);
-  for (var i = 0; i < imageData.data.length; i+=4) {
-    imageData.data[i] = 255 - imageData.data[i];
-    imageData.data[i+1] = 255 - imageData.data[i+1];
-    imageData.data[i+2] = 255 - imageData.data[i+2];
-  }
-  ctx.putImageData(imageData, 0, 0);
+  var ctx = document.getElementsByTagName("canvas")[0].getContext('2d');
+  var imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+	for (var i = 0; i < imageData.data.length; i += 4) {
+		imageData.data[i] = 255 - imageData.data[i];
+		imageData.data[i + 1] = 255 - imageData.data[i + 1];
+		imageData.data[i + 2] = 255 - imageData.data[i + 2];
+	}
+	ctx.putImageData(imageData, 0, 0);
 }
 
-document.getElemenById('invert-colors-btn').addEventListener("click", invertColorButtonClick;
+document.getElementById("invert-colors-btn").addEventListener("click", invertColorButtonClick);
+
