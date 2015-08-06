ba5c5d2cb75c0f39ae8ff90bfafecbce2ed80652
aliv59 <vilavaln@gmail.com>
2015-08-06 22:45:11 +0300

    new Exam

---
+++ Exam/ExamAug2015/Task1/tasks/task-1.js
@@ -0,0 +1,14 @@
+function solve() {
+    return function (selector, isCaseSensitive) {
+        var root = document.querySelector(selector);
+        isCaseSensitive = isCaseSensitive || false;
+
+
+
+
+
+
+  };
+}
+
+module.exports = solve;
